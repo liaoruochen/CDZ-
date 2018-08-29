63c56398b17a6c8bf81d310284700596625a2091
“liaoruochen” <920037556@qq.com>
2018-08-29 14:02:04 +0800

    初始化项目

---
+++ cdz/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  created () {
+    // 调用API从本地缓存中获取数据
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ cdz/src/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <p>温馨提示</p>
+    <p>请允许微信授权以使用更多功能</p>
+    <button @click="getUserInfo">确定</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      userInfo: {}
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+      console.log('22')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    
+  }
+</style>
