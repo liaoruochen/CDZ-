0bd7f7a8b6a37eacfe8833ac3be6f337533ac010
“liaoruochen” <920037556@qq.com>
2018-09-04 15:35:22 +0800

    切图

--- cdz/src/pages/money/index.vue
+++ cdz/src/pages/money/index.vue
@@ -1,15 +1,55 @@
 <template>
-  <div class="container">
-    
+  <div class="weui-cells">
+    <div class="box">
+      <p class="yuer">账户余额</p>
+      <p class="money">500.00元</p>
+      <div class="recharge">
+        <a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_primary">充值</a>
+      </div>
+    </div>
+    <a class="weui-cell weui-cell_access" href="javascript:;">
+      <div class="weui-cell__bd">
+          <p>消费明细</p>
+      </div>
+      <div class="weui-cell__ft">
+      </div>
+    </a>
+    <a class="weui-cell weui-cell_access" href="javascript:;">
+      <div class="weui-cell__bd">
+          <p>充值记录</p>
+      </div>
+      <div class="weui-cell__ft">
+      </div>
+    </a>
   </div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+  .box {
+    height: 180rpx;
 
+    .yuer {
+      font-size: 38rpx;
+      text-align: center;
+      margin-top: 10rpx;
+    }
+    .money {
+      font-size: 28rpx;
+      text-align: center;
+    }
+    .recharge {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .weui-btn {
+      margin: 0 auto;
+      margin-top: 15rpx;
+    }
+  }
 </style>
---
+++ cdz/src/pages/recharge/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="weui-cells">
+      <div class="weui-cell">
+        <div class="weui-cell__hd"><label class="weui-label">金额（元）</label></div>
+        <div class="weui-cell__bd">
+            <input class="weui-input" type="number" placeholder="请输入充值金额">
+        </div>
+      </div>
+    </div>
+    <button :disabled="isAgree" class="weui-btn weui-btn_primary">充值</button>
+    <div class="footer">
+      <input type="checkbox" v-model="ischeck" :checked = true @click="check" />
+      <p>点击立即充值，即表示您已阅读并同意<span class="xieyi">《充值协议》</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isAgree: false,
+      ischeck: ''
+    }
+  },
+  methods: {
+    check () {
+      if (this.isAgree === true) {
+        this.isAgree = false
+      } else {
+        this.isAgree = true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .footer {
+    width: 100%;
+    height: 70rpx;
+    position: absolute;
+    bottom: 80rpx;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    p {
+      font-size: 25rpx;
+
+      .xieyi {
+        color: green
+      }
+    }
+  }
+</style>
