6f89a4e422bca1cb3749503f1d92b2dc06cd7c21
“liaoruochen” <920037556@qq.com>
2018-09-05 11:00:21 +0800

    切图

---
+++ cdz/src/pages/opinion/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <div class="weui-cells weui-cells_form">
+    <div class="weui-cell">
+        <div class="weui-cell__bd">
+            <textarea class="weui-textarea" placeholder="请输入意见建议..." rows="3"></textarea>
+        </div>
+    </div>
+  </div>
+  <button class="weui-btn weui-btn_primary">提交</button>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ cdz/src/pages/record/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="weui-cells">
+    <div class="weui-cell">
+      <div class="weui-cell__bd">
+          <p>2018年8月12日19:00</p>
+      </div>
+      <div class="weui-cell__ft">30.00元</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
