c19645afd35b4925f6e857ba12d92cf61880e0fd
WsmDyj <972774037@qq.com>
2018-09-11 21:28:10 +0800

    充电界面

--- cdz/src/pages/charge/index.vue
+++ cdz/src/pages/charge/index.vue
@@ -1,15 +1,70 @@
 <template>
-  <div>
-    
+  <div class="charge">
+    <div class="charge__title">扫描充电桩上的二维码开启充电</div>
+    <div class="charge_desc" @click="resgister">
+      <img src="../../../static/img/about1.png" style="width:30rpx;height:30rpx;" alt="提示">
+      <span style="padding-left:10rpx">需要注册才能扫码充电 ></span>
+    </div>
+    <div class="charge__pic" @click="scavenging">
+      <img src="../../../static/img/scavenging.png" style="width:200rpx;height:200rpx" alt="扫码">
+    </div>
+    <div></div>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    scavenging () {
+      wx.scanCode({
+        success: function (res) {
+          console.log(res)
+        }
+      })
+    },
+    resgister () {
+      wx.navigateTo({
+        url: '/pages/resgister/main',
+        success: function (res) {
+          // success
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.charge {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  .charge__title {
+    padding-top: 100rpx;
+    font-size: 36rpx;
+    font-weight: 500;
+    text-align: center;
+  }
+  .charge_desc {
+    font-size: 30rpx;
+    margin-top: 40rpx;
+    color: #555555;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .charge__pic {
+    width: 200rpx;
+    height: 200rpx;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    animation: hovertreemove 1s linear infinite alternate;
+  }
+  @keyframes hovertreemove {
+    from {top:50%;}
+    to {top:55%;}
+  }
+}
 </style>
--- cdz/src/pages/index/index.vue
+++ cdz/src/pages/index/index.vue
@@ -1,26 +1,46 @@
 <template>
-  <div class="comtainer">
+  <div class="comtainer" v-show="userInfo">
     <div class="box">
       <p class="remind">温馨提示</p>
       <p class="please">请允许微信授权以使用更多功能</p>
-      <button open-type='getUserInfo' class="weui-btn weui-btn_plain-primary btn">确定</button>
+      <button open-type='getUserInfo' class="weui-btn weui-btn_plain-primary btn" @bindgetuserinfo="bindGetUserInfo">确定</button>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   data () {
     return {
-      userInfo: {}
+      userInfo: true,
+      canIUse: wx.canIUse('button.open-type.getUserInfo')
     }
   },
-
-  components: {
+  onLoad: function () {
+    console.log(1)
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.record']) {
+          this.userInfo = false
+          wx.switchTab({
+            url: 'main'
+          })
+        }
+      }
+    })
   },
-
+  components: {},
   methods: {
+    bindGetUserInfo: function (e) {
+      console.log(e)
+      if (e.detail.userInfo) {
+        // 用户按了允许授权按钮
+        console.log('yunxu')
+      } else {
+        // 用户按了拒绝按钮
+        console.log('quxiao')
+      }
+    }
   }
 }
 </script>
@@ -49,7 +69,7 @@ export default {
       text-align: center;
       font-size: 28rpx;
       margin-top: 20rpx;
-      color: #888888
+      color: #888888;
     }
     .btn {
       position: absolute;
